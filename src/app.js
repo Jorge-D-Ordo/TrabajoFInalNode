03983b156d2b5d661cbfa638d6f8dacb8827fb5a
Jorge-D-Ordo <jorge5d5ordo5@gmail.com>
2026-07-04 16:18:55 -0300

    problemas por falta de codigo

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-*app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ mensaje: "API funcionando correctamente" });
 });
 
