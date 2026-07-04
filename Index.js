176b64ea7f35a7bd26e0afb9c3e2be814ac693f1
Jorge-D-Ordo <jorge5d5ordo5@gmail.com>
2026-07-04 14:11:07 -0300

    Repareacion de index y vercel correccion error 404

--- Index.js
+++ Index.js
@@ -1,15 +1,19 @@
 import app from "./src/app.js";
-import dotenv from "dotenv";
+/* import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-/*                      borrar  ********************************************************************* */
-console.log("Programa iniciado");
+
+
 
 
 dotenv.config();
 app.listen(PORT, () => {
   console.log(`Servidor en el puerto ${PORT}`);
-});
+})
+ borrar  ********************************************************************* */
+console.log("Programa iniciado");
+export default app;
+;
