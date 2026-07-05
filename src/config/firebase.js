9990d2aa3f4f40f90eebbd2c7d0094c6e05a1de3
Jorge-D-Ordo <jorge5d5ordo5@gmail.com>
2026-07-05 11:38:59 -0300

    agregado codigo firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,8 +9,13 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: "trabajofinalbackendnode.firebasestorage.app",
+  messagingSenderId: "718457598724",
+  appId: "1:718457598724:web:c29f1881ad292da8ac9651",
+  measurementId: "G-58MNQ7R1R7"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
